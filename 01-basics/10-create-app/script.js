b126ae0d4ff603a9be7641bc967dad226e10648b
Kira <kira-ismailova@bk.ru>
2024-12-19 15:42:37 +0300

    Создание компонента с сегодняшней датой

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,32 @@
-import { defineComponent, createApp } from 'vue'
+import { defineComponent, createApp } from 'vue';
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+    function formattedData() {
+      const date = new Date();
+    
+      const options = { 
+        dateStyle: 'long',
+      };
+      
+      return date.toLocaleDateString(navigator.language, options);
+    }
+
+    const DATE = formattedData();
+
+    return {
+      formattedData,
+      DATE,
+    }
+  },
+
+  template: `
+    <div>Сегодня {{ DATE }}</div>
+  `
+})
+
+const app = createApp(App);
+const vm = app.mount('#app');
+window.vm = vm;
